fa31ab0f9cfdc49467b79665db66e0868dfae3ec
tanknew <tanknew@outlook.com>
2019-11-10 21:57:24 +0800

    mac

--- src/main.js
+++ src/main.js
@@ -49,54 +49,72 @@ if (!abp.utils.getCookieValue('Abp.Localization.CultureName')) {
     )
 }
 
-router.beforeEach((to, from, next) => {
-    store.commit('setToken', getToken())
-    store.commit('setUser', getUerFromLocalStorage())
-    if (to.matched.some(m => m.meta.auth)) {
-        if (!store.getters.hastoken) next({ path: '/login', query: { Rurl: to.fullPath } })
-        else if (!store.getters.isTokenExpired) next()
-        else {
-            // 未登录则跳转到登陆界面，query:{ Rurl: to.fullPath}表示把当前路由信息传递过去方便登录后跳转回来；
-            Ajax.get(tools.tokenUrl + '/RefreshToken')
-                .then(function(response) {
-                    var json = response.data
-                    if (json.success === true) {
-                        let result = json.result
-                        let token = {
-                            AccessToken: result.accessToken,
-                            EncryptedAccessToken: result.encryptedAccessToken,
-                            ExpireInSeconds: result.expireInSeconds,
-                            RefreshToken: result.refreshToken
-                        }
-                        store.commit('setToken', token)
-                        store.commit('setUser', jwtDecode(token.AccessToken))
-                        setToken(token)
-                    } else next({ path: '/login', query: { Rurl: to.fullPath } })
-                })
-                .then(() => next())
-                .catch(() => next({ path: '/login', query: { Rurl: to.fullPath } }))
-        }
-    } else {
-        next()
-    }
-})
-
 if (!window.localStorage) {
     alert('This browser do not supports localStorage. Please change browser to ie 9.0 at least .')
 }
-// store.commit('setToken', getToken())
-// store.commit('setUser', getUerFromLocalStorage())
+store.commit('setToken', getToken())
+store.commit('setUser', getUerFromLocalStorage())
 // 设置一个防伪令牌
-Ajax.get('/api/AntiForgery/GetToken').then(() =>
-    Ajax.get('/AbpUserConfiguration/GetAll').then(data => {
-        window.abp = tools.extend(true, window.abp, data.data.result)
-        new Vue({
-            el: '#app',
-            router,
-            store,
-            components: { App },
-            template: '<App/>',
-            created() {}
-        })
-    })
-)
+Ajax.get('/api/AntiForgery/GetToken').then(() => {
+    if (!!store.getters.hastoken && store.getters.isTokenExpired)
+        Ajax.get(tools.tokenUrl + '/RefreshToken')
+            .then(function(response) {
+                var json = response.data
+                if (json.success === true) {
+                    let result = json.result
+                    let token = {
+                        AccessToken: result.accessToken,
+                        EncryptedAccessToken: result.encryptedAccessToken,
+                        ExpireInSeconds: result.expireInSeconds,
+                        RefreshToken: result.refreshToken
+                    }
+                    store.commit('setToken', token)
+                    store.commit('setUser', jwtDecode(token.AccessToken))
+                    setToken(token)
+                } else next({ path: '/login', query: { Rurl: to.fullPath } })
+            })
+            .then(() =>
+                Ajax.get('/AbpUserConfiguration/GetAll').then(data => {
+                    window.abp = tools.extend(true, window.abp, data.data.result)
+                    router.beforeEach((to, from, next) => {
+                        store.commit('setToken', getToken())
+                        store.commit('setUser', getUerFromLocalStorage())
+                        if (to.matched.some(m => m.meta.auth)) {
+                            if (!store.getters.hastoken) next({ path: '/login', query: { Rurl: to.fullPath } })
+                            else if (!store.getters.isTokenExpired) next()
+                            else {
+                                // 未登录则跳转到登陆界面，query:{ Rurl: to.fullPath}表示把当前路由信息传递过去方便登录后跳转回来；
+                                Ajax.get(tools.tokenUrl + '/RefreshToken')
+                                    .then(function(response) {
+                                        var json = response.data
+                                        if (json.success === true) {
+                                            let result = json.result
+                                            let token = {
+                                                AccessToken: result.accessToken,
+                                                EncryptedAccessToken: result.encryptedAccessToken,
+                                                ExpireInSeconds: result.expireInSeconds,
+                                                RefreshToken: result.refreshToken
+                                            }
+                                            store.commit('setToken', token)
+                                            store.commit('setUser', jwtDecode(token.AccessToken))
+                                            setToken(token)
+                                        } else next({ path: '/login', query: { Rurl: to.fullPath } })
+                                    })
+                                    .then(() => next())
+                                    .catch(() => next({ path: '/login', query: { Rurl: to.fullPath } }))
+                            }
+                        } else {
+                            next()
+                        }
+                    })
+                    new Vue({
+                        el: '#app',
+                        router,
+                        store,
+                        components: { App },
+                        template: '<App/>',
+                        created() {}
+                    })
+                })
+            )
+})
